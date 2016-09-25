929ac129187284f959e5cfd043b51e31763c007d
skyguy94 <skyguy94@gmail.com>
2016-09-25 10:04:37 -0500

    Add checksums. remove depricated error handling. Use auto-uninstaller

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,29 +1,18 @@
-$package = 'SQL2012.ClrTypes'
+$params = @{
+  packageName = 'sql2012.clrtypes';
+  fileType = 'msi';
+  silentArgs = '/quiet';
+  url = 'http://download.microsoft.com/download/4/B/1/4B1E9B0E-A4F3-4715-B417-31C82302A70A/ENU/x86/SQLSysClrTypes.msi';
+  checksum='318967B11ED6C3D89C49415AB6D290E8A5BB38D0BC1954226D56D2A6A5A7C7E8';
+  checksumType='sha256';
+}
 
-try {
-  $params = @{
-    packageName = $package;
-    fileType = 'msi';
-    silentArgs = '/quiet';
-    url = 'http://download.microsoft.com/download/4/B/1/4B1E9B0E-A4F3-4715-B417-31C82302A70A/ENU/x86/SQLSysClrTypes.msi';
-    url64bit = 'http://download.microsoft.com/download/4/B/1/4B1E9B0E-A4F3-4715-B417-31C82302A70A/ENU/x64/SQLSysClrTypes.msi';
-  }
+Install-ChocolateyPackage @params
 
+if (Get-ProcessorBits -eq 64) {
+  $params['url'] = ''
+  $params['url64'] = 'http://download.microsoft.com/download/4/B/1/4B1E9B0E-A4F3-4715-B417-31C82302A70A/ENU/x64/SQLSysClrTypes.msi'
+  $params['checksum64'] ='5654DF31040A69104BDD47B2C34AFDD85BADA67A63B87BE3E90C82477A377D9C'
+  $params['checksumType64'] = 'sha256';
   Install-ChocolateyPackage @params
-
-  # http://forums.iis.net/p/1174672/1968094.aspx
-  # it turns out that even on x64, x86 clr types should also be installed
-  # or SMO breaks
-  $IsSytem32Bit = (($Env:PROCESSOR_ARCHITECTURE -eq 'x86') -and `
-    ($Env:PROCESSOR_ARCHITEW6432 -eq $null))
-  if (!$IsSytem32Bit)
-  {
-    $params.url64bit = $params.url
-    Install-ChocolateyPackage @params
-  }
-
-  Write-ChocolateySuccess $package
-} catch {
-  Write-ChocolateyFailure $package "$($_.Exception.Message)"
-  throw
 }
